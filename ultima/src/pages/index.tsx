2b4881446742f9313ba0226825bb9b2c9fb6a2fc
sdbarlow <sethdbarlow@gmail.com>
2023-04-29 08:41:02 -0600

    more carousel work

--- ultima/src/pages/index.tsx
+++ ultima/src/pages/index.tsx
@@ -15,34 +15,47 @@ export default function Home() {
               </div>
               </div>
             <div className="flex justify-center items-center w-screen h-screen overflow-hidden bg-black">
-            <div className="w-full max-w-3xl max-h-fit h-full flex justify-center flex-col items-center ">
+            <div className="w-full max-w-3xl max-h-fit h-4/5 flex justify-center flex-col items-center ">
+              <h1 className='text-white text-6xl'>Browse By Make</h1>
               <input type="radio" name="slider" id="item-1"/>
               <input type="radio" name="slider" id="item-2"/>
               <input type="radio" name="slider" id="item-3"/>
             <div className="cards">
               <label className="absolute w-3/5 h-full left-0 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" htmlFor="item-1" id="item-1-photo">
-                <img className="w-full h-full rounded-xl object-cover" src="/LamborghiniLogo.png" alt="song"/>
+                <img className="w-fit h-fit object-cover" src="/LamborghiniLogo.png" alt="song"/>
               </label>
-              <label className="absolute w-3/5 h-full left-0 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" htmlFor="item-2" id="item-2-photo">
-                <img className="w-full h-full rounded-xl object-cover" src="/PorscheLogo.png" alt="song"/>
+              <label className="absolute w-7/12 h-full left-0 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" htmlFor="item-2" id="item-2-photo">
+                <img className="w-fit h-fit object-cover" src="/PorscheLogo.png" alt="song"/>
               </label>
-              <label className="absolute w-3/5 h-full left-0 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" htmlFor="item-3" id="item-3-photo">
-                <img className="w-full h-full rounded-xl object-cover" src="/KoenigseggLogo.png" alt="song"/>
+              <label className="absolute w-4/6 h-full left-0 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" htmlFor="item-3" id="item-3-photo">
+                <img className="w-fit h-fit object-cover" src="/KoenigseggLogo.png" alt="song"/>
+              </label>
+              <label className="absolute w-3/5 h-full left-0 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" htmlFor="item-4" id="item-4-photo">
+                <img className="w-fit h-fit object-cover" src="/Ferrar.png" alt="song"/>
+              </label>
+              <label className="absolute w-3/5 h-full left-0 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" htmlFor="item-5" id="item-5-photo">
+                <img className="w-fit h-fit object-cover" src="/Mercedes.png" alt="song"/>
               </label>
             </div>
-            <div id="player" className="w-full max-w-3xl max-h-fit h-1/5 flex justify-center flex-col items-center ">
+            <div id="player" className="w-full max-w-3xl max-h-fit h-1/5 flex justify-center items-center ">
               <input type="radio" name="text-slider" id="item-text-1" />
               <input type="radio" name="text-slider" id="item-text-2"/>
               <input type="radio" name="text-slider" id="item-text-3"/>
             <div className="texts">
-              <label className="absolute w-3/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-1-text" htmlFor="item-1">
-                <h1 className="text-white text-4xl object-cover">Lamborghini</h1>
+              <label className="absolute w-1/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-1-text" htmlFor="item-1">
+                <h1 className="text-white text-center text-4xl object-cover">Lamborghini</h1>
               </label>
-              <label className="absolute w-3/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-2-text" htmlFor="item-2">
-                <h1 className="text-white text-4xl object-cover">Porsche</h1>
+              <label className="absolute w-1/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-2-text" htmlFor="item-2">
+                <h1 className="text-white text-center text-4xl object-cover">Porsche</h1>
               </label>
-              <label className="absolute w-3/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-3-text" htmlFor="item-3">
-                <h1 className="text-white text-4xl object-cover">Koenigsegg</h1>
+              <label className="absolute w-1/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-3-text" htmlFor="item-3">
+                <h1 className="text-white text-center text-4xl object-cover">Koenigsegg</h1>
+              </label>
+              <label className="absolute w-1/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-4-text" htmlFor="item-4">
+                <h1 className="text-white text-center text-4xl object-cover">Ferrari</h1>
+              </label>
+              <label className="absolute w-1/5 h-10 right-0 m-auto transition-transform duration-500 hover:cursor-pointer" id="item-5-text" htmlFor="item-5">
+                <h1 className="text-white text-center text-4xl object-cover">Mercedes</h1>
               </label>
             </div>
                 </div>
